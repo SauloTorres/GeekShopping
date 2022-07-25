db4f1d8c57313145affb8afb1952165bc99fe37a
SauloSantosSescBA <ba9132@sescbahia.com.br>
2022-07-25 09:51:45 -0300

    first commit

---
+++ GeekShopping.ProductAPI/Model/Context/SqlServerContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class SqlServerContext : DbContext
+    {
+        public SqlServerContext() :base()
+        {
+
+        }
+        public SqlServerContext(DbContextOptions<SqlServerContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => 
+            options.UseSqlServer("Server=localhost\\SQLEXPRESS; Database=geek_shopping_product_api; Integrated Security=False; MultipleActiveResultSets=true; User ID=sa;Password=33042917aA.");
+
+    }
+}
+
